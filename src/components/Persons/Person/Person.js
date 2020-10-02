28de4bf56bdcc01f565e310910d9019e3bb0ff6e
Stavros Vergos <stavrospvergos@gmail.com>
2020-10-02 18:49:58 +0300

    changed structure

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,7 +4,6 @@ import classes from './Person.css';
 
 const person = ( props ) => {
     return (
-        // <div className="Person" style={style}>
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!
             </p>
